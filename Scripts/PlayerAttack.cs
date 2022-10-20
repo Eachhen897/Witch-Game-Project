0567ebb2aa68c29aefbf6c50732e1c01cf5f6f64
MekhiVaughn <60224258+Eachhen897@users.noreply.github.com>
2022-10-20 17:59:00 -0400

    Scripts for the game

---
+++ Scripts/PlayerAttack.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Transform firePosition;
+    public GameObject projectile;
+
+    // Update is called once per frame
+    void Update()
+    {
+     if (Input.GetKeyDown("v"))
+        {
+            Instantiate(projectile, firePosition.position, firePosition.rotation);
+        }
+
+        
+    }
+}
